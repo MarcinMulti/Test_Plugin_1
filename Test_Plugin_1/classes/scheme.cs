f95c343ce11ad678a214afd6397260f09447f216
Marcin Luczkowski <marcl@multiconsult.no>
2023-07-02 18:16:28 +0200

    nowe klasy

---
+++ Test_Plugin_1/classes/scheme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino.Geometry;
+
+
+namespace Test_Plugin_1.classes
+{
+    internal class scheme
+    {
+        int id;
+        string name;
+
+        List<Curve> drawing;
+    }
+}
---
+++ Test_Plugin_1/classes/structure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino.Geometry;
+
+namespace Test_Plugin_1.classes
+{
+    internal class structure
+    {
+        int id;
+        string name;
+
+        string description;
+        Brep geometry;
+
+        scheme scheme;
+
+
+    }
+}
